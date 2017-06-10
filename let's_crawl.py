ef591286e3212681350ef76620bd33aa7fed048d
casol <frombitstobytes.com@gmail.com>
2017-06-10 17:47:14 +0200

    Traverse a single domain
    getLinks function takes in an URL and returns list of article
    based on the provided parameters. Main body loop randomly choose from
    a list link tag and then getting a new list of links for new article

--- let's_crawl.py
+++ let's_crawl.py
@@ -1,10 +1,30 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import datetime
+import re
+import random
+
+random.seed(datetime.datetime.now())
 
 
-html = urlopen("https://frombitstobytes.com/about/")
-bsObj = BeautifulSoup(html, 'html.parser')
-for link in bsObj.findAll("a"):
-    if 'href' in link.attrs:  # attrs <- accessing attributes
-        # dictionary object
-        print(link.attrs['href'])
+def getLinks(articleUrl):
+    try:
+        html = urlopen("http://en.wikipedia.org"+articleUrl)
+    except HTTPError as err:
+        return None
+    try:
+        bsObj = BeautifulSoup(html, "html.parser")
+        page_links = bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+    except AttributeError as err:
+        return None
+    return page_links
+
+links = getLinks("/wiki/Kevin_Bacon")
+if links is None:
+    print('Page or tag could not be found')
+else:
+    while len(links) > 0:
+        newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
